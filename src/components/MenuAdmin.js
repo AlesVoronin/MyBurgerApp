95c3f8d7abbc425304464c8e95e60ac7624cb55a
AlesVoronin <voronin@mkts.by>
2021-07-12 16:59:23 +0300

    feat: added insert burger form

--- src/components/MenuAdmin.js
+++ src/components/MenuAdmin.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AddBurgerForm from './AddBurgerForm';
+
 
 class MenuAdmin extends React.Component {
     render() {
         return (
             <div className='order-wrap'>
                 <h2>Управление Меню</h2>
+                <AddBurgerForm addBurger={this.props.addBurger}/>
             </div>
         );
     }
